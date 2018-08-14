ec1fe75c4d8b85aaeb2b854ca85d8abc282290e5
Alexander Schrode <Midix01@googlemail.com>
2018-08-14 10:15:54 +0200

    Removed warnings for msvc

--- toml.c
+++ toml.c
@@ -173,63 +173,63 @@ int toml_ucs_to_utf8(int64_t code, char buf[6])
     */
     if (code < 0) return -1;
     if (code <= 0x7F) {
-	buf[0] = (unsigned char) code;
-	return 1;
+    buf[0] = (unsigned char) code;
+    return 1;
     }
 
     /* 0x00000080 - 0x000007FF:
        110xxxxx 10xxxxxx
     */
     if (code <= 0x000007FF) {
-	buf[0] = 0xc0 | (code >> 6);
-	buf[1] = 0x80 | (code & 0x3f);
-	return 2;
+    buf[0] = (char) (0xc0 | (code >> 6));
+    buf[1] = (char) (0x80 | (code & 0x3f));
+    return 2;
     }
 
     /* 0x00000800 - 0x0000FFFF:
        1110xxxx 10xxxxxx 10xxxxxx
     */
     if (code <= 0x0000FFFF) {
-	buf[0] = 0xe0 | (code >> 12);
-	buf[1] = 0x80 | ((code >> 6) & 0x3f);
-	buf[2] = 0x80 | (code & 0x3f);
-	return 3;
+    buf[0] = (char) (0xe0 | (code >> 12));
+    buf[1] = (char) (0x80 | ((code >> 6) & 0x3f));
+    buf[2] = (char) (0x80 | (code & 0x3f));
+    return 3;
     }
 
     /* 0x00010000 - 0x001FFFFF:
        11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
     */
     if (code <= 0x001FFFFF) {
-	buf[0] = 0xf0 | (code >> 18);
-	buf[1] = 0x80 | ((code >> 12) & 0x3f);
-	buf[2] = 0x80 | ((code >> 6) & 0x3f);
-	buf[3] = 0x80 | (code & 0x3f);
-	return 4;
+    buf[0] = (char) (0xf0 | (code >> 18));
+    buf[1] = (char) (0x80 | ((code >> 12) & 0x3f));
+    buf[2] = (char) (0x80 | ((code >> 6) & 0x3f));
+    buf[3] = (char) (0x80 | (code & 0x3f));
+    return 4;
     }
 
     /* 0x00200000 - 0x03FFFFFF:
        111110xx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
     */
     if (code <= 0x03FFFFFF) {
-	buf[0] = 0xf8 | (code >> 24);
-	buf[1] = 0x80 | ((code >> 18) & 0x3f);
-	buf[2] = 0x80 | ((code >> 12) & 0x3f);
-	buf[3] = 0x80 | ((code >> 6) & 0x3f);
-	buf[4] = 0x80 | (code & 0x3f);
-	return 5;
+    buf[0] = (char) (0xf8 | (code >> 24));
+    buf[1] = (char) (0x80 | ((code >> 18) & 0x3f));
+    buf[2] = (char) (0x80 | ((code >> 12) & 0x3f));
+    buf[3] = (char) (0x80 | ((code >> 6) & 0x3f));
+    buf[4] = (char) (0x80 | (code & 0x3f));
+    return 5;
     }
     
     /* 0x04000000 - 0x7FFFFFFF:
        1111110x 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
     */
     if (code <= 0x7FFFFFFF) {
-	buf[0] = 0xfc | (code >> 30);
-	buf[1] = 0x80 | ((code >> 24) & 0x3f);
-	buf[2] = 0x80 | ((code >> 18) & 0x3f);
-	buf[3] = 0x80 | ((code >> 12) & 0x3f);
-	buf[4] = 0x80 | ((code >> 6) & 0x3f);
-	buf[5] = 0x80 | (code & 0x3f);
-	return 6;
+    buf[0] = (char) (0xfc | (code >> 30));
+    buf[1] = (char) (0x80 | ((code >> 24) & 0x3f));
+    buf[2] = (char) (0x80 | ((code >> 18) & 0x3f));
+    buf[3] = (char) (0x80 | ((code >> 12) & 0x3f));
+    buf[4] = (char) (0x80 | ((code >> 6) & 0x3f));
+    buf[5] = (char) (0x80 | (code & 0x3f));
+    return 6;
     }
 
     return -1;
